ee45a20d0aecd4d98e6199912565fa0cf89ee57b
dwatrous <dwmaillist@gmail.com>
2015-09-17 11:55:12 -0500

    commenting optional commands in bootstrap script

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export https_proxy=https://proxy.houston.hp.com:8080
-export http_proxy=http://proxy.houston.hp.com:8080
+#export https_proxy=https://proxy.company.com:8080
+#export http_proxy=http://proxy.company.com:8080
 
 apt-get update
 sudo apt-get install -y unzip python-pip python-dev
